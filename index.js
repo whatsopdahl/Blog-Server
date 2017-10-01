8f58686514e5417dd50062aa2a94e652f29c254b
whatsopdahl <jopdahl12@gmail.com>
2017-09-30 20:56:33 -0500

    enable CORS

--- index.js
+++ index.js
@@ -13,6 +13,11 @@ const transporter = mailer.createTransport({
 
 api.set('port', (process.env.port || 3000));
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 api.use(bodyParser.urlencoded({
   extended : true
 }));
