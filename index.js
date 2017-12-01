1da42d0f40da94821c73f56f84a19a80995867ae
whatsopdahl <jopdahl12@gmail.com>
2017-11-30 21:03:37 -0600

    try to handle options method

--- index.js
+++ index.js
@@ -18,7 +18,8 @@ api.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "http://whatsopdahl.com");
   res.header("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
+  if (req.method == 'OPTIONS') res.send(200);
+  else next();
 });
 api.use(bodyParser.urlencoded({
   extended : true
